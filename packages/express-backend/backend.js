846d5ff6789794e558b98bfb30e56057322551ac
Chris Morales <chris.morales1102@gmail.com>
2026-01-21 19:58:07 -0800

    part 5 done

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,13 +1,6 @@
 // backend.js
 import express from "express";
 
-const findUserByName = (name) => {
-  return users.users_list.filter(
-    (user) => user.name === name
-  );
-};
-
-
 const users = {
   users_list: [
     {
@@ -44,14 +37,22 @@ const port = 8000;
 
 app.use(express.json());
 
-app.get("/users", (req, res) => {
-  const name = req.query.name;
+const findUserByName = (name) => {
+  return users.users_list.filter(
+    (user) => user.name === name
+  );
+};
 
-  if (name !== undefined) {
-    let result = findUserByName(name);
-    res.send({ users_list: result });
+const findUserById = (id) =>
+    users.users_list.find((user) => user.id === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
   } else {
-    res.send(users);
+    res.send(result);
   }
 });
 
